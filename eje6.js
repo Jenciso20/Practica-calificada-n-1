966a37c5978577c44457a3198d110c391983a3bc
Jenciso20 <jencisoquispe@gmail.com>
2024-07-17 22:29:21 -0500

    ejercicio 6

---
+++ eje6.js
@@ -0,0 +1,5 @@
+/*
+6.- El jugador con más puntos:
+Dada una lista de tres jugadores con sus respectivas puntuaciones (jugador1,
+jugador2, jugador3), determina quién de ellos tiene la puntuación más alta.
+*/
